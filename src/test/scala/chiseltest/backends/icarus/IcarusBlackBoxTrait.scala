a7742ecd5f078468507255d72f94d3e9e12a751b
andrejstrogonov <strogonovandrej@hotmail.com>
2024-11-20 22:20:05 +0300

    Discoving a lot of mistakes in ChiselTest

--- src/test/scala/chiseltest/backends/icarus/IcarusBlackBoxTrait.scala
+++ src/test/scala/chiseltest/backends/icarus/IcarusBlackBoxTrait.scala
@@ -1,24 +1,30 @@
 package chiseltest.backends.icarus
 
-import chisel3.{Bundle, IO, Input, Output, UInt, fromIntToWidth}
+import chisel3.DontCare.:=
+import chisel3.simulator.PeekPokeAPI.testableData
+import chisel3.{Bundle, IO, Input, Output, UInt, fromIntToLiteral, fromIntToWidth}
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.must.Matchers.be
 import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
 import org.scalatestplus.mockito.MockitoSugar.mock
 
+import scala.util.Random
+
 trait IcarusBlackBoxTrait {
-    val io: Bundle = IO(new Bundle {
         val a: UInt = Input(UInt(8.W))
         val b: UInt = Input(UInt(8.W))
         val q: UInt = Output(UInt(8.W))
-    })
 }
-class TestBlackBox extends AnyFunSuite with MockFactory{
+class TestBlackBox extends AnyFunSuite with MockFactory {
   val blackBox = mock[IcarusBlackBoxTrait]
 
-  test("BlackBox should be able to add two numbers") {
-    blackBox.io.a := 10.U
-    blackBox.io.b := 20.U
+  test("support IcarusVerilog black boxes") {
+   blackBox.io.a := 8.U
+   blackBox.io.b := 8.U
+   blackBox.io.q := 16.U
+    blackBox.io.q should be(convertToAnyShouldWrapper(16.U))
+  }
 }
 
 
