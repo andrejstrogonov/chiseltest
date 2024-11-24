0cfb6c5a1ef6329b3f577604503a9e1e2d1f93cf
andrejstrogonov <strogonovandrej@hotmail.com>
2024-11-24 20:05:00 +0300

    Add TreadleBlackBoxTestTrait.scala test

---
+++ src/test/scala/chiseltest/backends/treadle/TreadleBlackBoxTestTrait.scala
@@ -0,0 +1,16 @@
+package chiseltest.backends.treadle
+
+import org.scalamock.scalatest.MockFactory
+import org.scalatest.funsuite.AnyFunSuite
+
+trait TreadleBlackBoxTestTrait {
+
+}
+class TreadleBlackBoxTest extends AnyFunSuite with MockFactory{
+  // Your test cases go here
+  // For example:
+  // test("Simple circuit") {
+  //   val io = new SimpleCircuit()
+  //   assert(io.out === 0.U)
+  // }
+}
